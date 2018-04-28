b4646583609dea4ab076e2ff2ffe04c39e902c28
Tensai <jonathan.bengoa@gmail.com>
2018-04-28 16:40:21 -0300

    Note script updated

---
+++ Assets/Scripts/LevelFactory.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[System.Serializable]
+public class NoteInfo
+{
+    public float StartAt;
+    public TypeOfNotes NoteType;
+    public float Duration;
+    public GeometryDeformation[] Deformations;
+}
+
+public class LevelFactory : MonoBehaviour {
+
+    private const int POOL_SIZE = 15;
+
+    public float StartSongAt;
+    public NoteInfo[] NotesInfo;
+
+    private Note[] _notePool;
+    
+    void Start ()
+    {
+        _notePool = new Note[POOL_SIZE];
+	}
+	
+	
+}
+
--- Assets/Scripts/Note.cs
+++ Assets/Scripts/Note.cs
@@ -2,15 +2,64 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Note : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private TypeOfNotes _noteType;   
+    private MeshFilter _meshFilter;
+    private Renderer _renderer;
+
+    public TypeOfNotes NoteType
+    {
+        get
+        {
+            return _noteType;
+        }
+    }
+
+    private MeshFilter NoteMeshFilter
+    {
+        get
+        { 
+            if (_meshFilter == null)
+                _meshFilter = this.GetComponent<MeshFilter>();
+            return _meshFilter;
+        }
+    }
+
+    private Renderer NoteRenderer
+    {
+        get
+        {
+            if (_renderer == null)
+                _renderer = this.GetComponent<Renderer>();
+            return _renderer;
+        }
+    }
+
+    public void UpdateNote(NoteInfo noteInfo)
+    {
+        NoteMeshFilter.mesh = GeometryCreator.CreateNote(noteInfo.Duration, noteInfo.Deformations);
+        _noteType = noteInfo.NoteType;
+
+        Color newColor = Color.white;
+
+        switch (_noteType)
+        {
+            case TypeOfNotes.Blue:
+                newColor = Color.blue;
+                break;
+            case TypeOfNotes.Green:
+                newColor = Color.green;
+                break;
+            case TypeOfNotes.Red:
+                newColor = Color.red;
+                break;
+            case TypeOfNotes.Yellow:
+                newColor = Color.yellow;
+                break;
+
+        }
+        NoteRenderer.material.color = newColor;
+    }
 }
